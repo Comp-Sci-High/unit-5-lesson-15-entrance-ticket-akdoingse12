544caa8f2f3c80752533c6626f93f3bb38c70d28
akdoingse12 <akdoingse12@gmail.com>
2025-04-08 20:31:41 +0000

    woefko

--- public/script.js
+++ public/script.js
@@ -1,7 +1,11 @@
 // Create a function that tests the post route handler [2 pts]
-async function testPost()
 
 
 // Call the function [1 pt]
 // Show us the successful POST fetch [1 pt]
-
+async function getPotion(){
+    // fill in the path
+    const response = await fetch(base + "potion")
+    const data = await response.json()
+    console.log(data)
+}
